d9b0213711e4be275b324d5cf9e30ed6f556f5d7
Luis Alberto Beltrán Pérez <luisbeltranfw@gmail.com>
2020-05-13 00:51:07 -0600

    First commit

---
+++ src/components/auth/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <section className="section auth">
+      <div className="container">
+        <h1>Welcome!</h1>
+        <p>You have successfully registered a new account.</p>
+        <p>We've sent you an email. Please verify your account by clicking on the link in that mail. </p>
+      </div>
+    </section>
+  )
+}
